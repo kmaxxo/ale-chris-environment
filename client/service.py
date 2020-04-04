e1866e57c32a09ec95f72c9180e410b6c992b030
Jose M Camacho <josem.camacho@gmail.com>
2020-04-04 14:36:04 +1100

    parametrize entrypoint

--- client/service.py
+++ client/service.py
@@ -1,13 +1,15 @@
 import requests
 from flask import jsonify
 
+apiEntrypoint = 'http://ace-server:8888'
+
 class ToDoService:
     def create(self, params):
-        response = requests.post("http://ace-server:8888/todo", data = params).json()
+        response = requests.post(apiEntrypoint+"/todo", data = params).json()
         return response
 
     def list(self):
         #response = self.model.list_items()
         #return response
-        response = requests.get("http://ace-server:8888/todo").json()
+        response = requests.get(apiEntrypoint+"/todo").json()
         return response
